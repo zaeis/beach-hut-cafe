4adadb55633c0ca4b3ce128cfcaf56db86985ea5
Elsa Dinletir <edinletir@gmail.com>
2022-09-23 21:32:59 +0100

    test with drawer page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
+import alpine from '@astrojs/alpinejs';
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
   site: 'https://beach-hut-cafe.com',
   integrations: [
+    alpine(),
     preact(),
     sitemap({
       canonicalURL: 'https://beach-hut-cafe.com'
