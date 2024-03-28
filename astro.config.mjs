450c60bbf09535fb0e4998449996a3f427526a03
Elsa <edinletir@gmail.com>
2024-03-28 17:08:35 +0000

    easter page wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from "astro/config";
 import alpine from '@astrojs/alpinejs';
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
-
+// https://astro.build/config
 export default defineConfig({
   site: 'https://beach-hut-cafe.com',
-  integrations: [
-    alpine(),
-    preact(),
-    sitemap({
-      canonicalURL: 'https://beach-hut-cafe.com'
-    })
-  ],
+  integrations: [alpine(), preact(), sitemap({
+    canonicalURL: 'https://beach-hut-cafe.com'
+  }), react()]
 });
